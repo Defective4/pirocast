5e99816429f9412476214d8314c52ca0c4173708
Defective <def3ctive4@gmail.com>
2025-01-24 21:33:42 +0100

    Use primitive types in properties

--- src/main/java/io/github/defective4/rpi/pirocast/props/AppProperties.java
+++ src/main/java/io/github/defective4/rpi/pirocast/props/AppProperties.java
@@ -3,21 +3,21 @@ package io.github.defective4.rpi.pirocast.props;
 import java.util.Properties;
 
 public class AppProperties extends Properties {
-    protected String aprsResamplerPort = "55557";
-    protected String controllerPort = "55555";
-    protected String rdsPort = "55556";
+    protected int aprsResamplerPort = 55557;
+    protected int controllerPort = 55555;
+    protected int rdsPort = 55556;
     protected String receiverExecutablePath = "./src/main/grc/receiver.py";
 
     public int getAprsResamplerPort() {
-        return Integer.parseInt(aprsResamplerPort);
+        return aprsResamplerPort;
     }
 
     public int getControllerPort() {
-        return Integer.parseInt(controllerPort);
+        return controllerPort;
     }
 
     public int getRdsPort() {
-        return Integer.parseInt(rdsPort);
+        return rdsPort;
     }
 
     public String getReceiverExecutablePath() {
