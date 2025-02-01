0f8cd5716fcf756d15f2c27713f0672960f943bd
Defective <def3ctive4@gmail.com>
2025-02-01 17:38:47 +0100

    Save and load app properties

--- src/main/java/io/github/defective4/rpi/pirocast/Main.java
+++ src/main/java/io/github/defective4/rpi/pirocast/Main.java
@@ -1,5 +1,6 @@
 package io.github.defective4.rpi.pirocast;
 
+import java.io.File;
 import java.util.Arrays;
 
 import io.github.defective4.rpi.pirocast.props.AppProperties;
@@ -7,7 +8,19 @@ import io.github.defective4.rpi.pirocast.props.AppProperties;
 public class Main {
     public static void main(String[] args) {
         try {
-            AppProperties props = new AppProperties();
+            File propsFile = new File("pirocast.properties");
+            AppProperties props = new AppProperties(propsFile);
+
+            if (!propsFile.isFile()) {
+                props.save();
+                System.err
+                        .println(
+                                "Default properties saved to \"" + propsFile + "\". Review it and run Pirocast again.");
+                return;
+            }
+
+            props.load();
+
             Pirocast cast = new Pirocast(Arrays
                     .asList(new Source("FM", SignalMode.FM, 87e6f, 108e6f, 88e6f, null, true, false),
                             new Source("AUX", SignalMode.AUX, 0, 0, 0, null, false, false)),
--- src/main/java/io/github/defective4/rpi/pirocast/props/AppProperties.java
+++ src/main/java/io/github/defective4/rpi/pirocast/props/AppProperties.java
@@ -1,5 +1,12 @@
 package io.github.defective4.rpi.pirocast.props;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.lang.reflect.Field;
 import java.util.Locale;
 import java.util.Properties;
 
@@ -17,6 +24,12 @@ public class AppProperties extends Properties {
     protected int ui_longClickLength = 500;
     protected int ui_rdsScrollSpeed = 1;
     protected int ui_standbyDisplayLinger = 5000;
+
+    private final File file;
+
+    public AppProperties(File file) {
+        this.file = file;
+    }
 
     public int getAprsResamplerPort() {
         return aprsResamplerPort;
@@ -65,6 +78,37 @@ public class AppProperties extends Properties {
 
     public String getTimeFormat() {
         return timeFormat;
+    }
+
+    public void load() throws IOException {
+        if (file.isFile()) {
+            try (Reader reader = new FileReader(file)) {
+                load(reader);
+            }
+            for (Field field : getClass().getDeclaredFields()) {
+                try {
+                    String val = getProperty(field.getName().replace('_', '.'));
+                    if (val != null) {
+                        if (field.getType() == String.class) field.set(this, val);
+                        else if (field.getType() == int.class) field.set(this, Integer.parseInt(val));
+                    }
+                } catch (Exception e) {}
+            }
+        }
+    }
+
+    public void save() throws IOException {
+        for (Field field : getClass().getDeclaredFields()) try {
+            String newVal = null;
+            Object fVal = field.get(this);
+            if (fVal instanceof String str) newVal = str;
+            else if (fVal instanceof Integer i) newVal = Integer.toString(i);
+            if (newVal != null) setProperty(field.getName().replace('_', '.'), newVal);
+        } catch (Exception e) {}
+
+        try (Writer writer = new FileWriter(file)) {
+            store(writer, null);
+        }
     }
 
 }
