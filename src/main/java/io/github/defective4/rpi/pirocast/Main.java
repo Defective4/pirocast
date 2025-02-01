bf2ce374ebd0ad52173198d5466ea4f304ec88c8
Defective <def3ctive4@gmail.com>
2025-02-01 18:17:04 +0100

    Save and load sources configuration

--- src/main/java/io/github/defective4/rpi/pirocast/Main.java
+++ src/main/java/io/github/defective4/rpi/pirocast/Main.java
@@ -1,15 +1,60 @@
 package io.github.defective4.rpi.pirocast;
 
 import java.io.File;
-import java.util.Arrays;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import io.github.defective4.rpi.pirocast.props.AppProperties;
 
 public class Main {
     public static void main(String[] args) {
         try {
+            File sourcesFile = new File("sources.json");
             File propsFile = new File("pirocast.properties");
             AppProperties props = new AppProperties(propsFile);
+
+            if (!sourcesFile.exists()) {
+                try (InputStream in = Main.class.getResourceAsStream("/sources.json.default")) {
+                    Files.copy(in, sourcesFile.toPath());
+                    System.err.println("Default sources saved to " + sourcesFile);
+                }
+            }
+
+            List<Source> sources = new ArrayList<>();
+
+            if (sourcesFile.isFile()) try (Reader reader = new FileReader(sourcesFile)) {
+                Gson gson = new Gson();
+                JsonArray array = JsonParser.parseReader(reader).getAsJsonObject().getAsJsonArray("sources");
+                for (JsonElement el : array) {
+                    if (el instanceof JsonObject obj) {
+                        try {
+                            SourceConfig srcCfg = gson.fromJson(obj, SourceConfig.class);
+                            String ivField = srcCfg.isValid();
+                            if (ivField != null) throw new IOException("Field " + ivField + " is missing or invalid");
+                            sources.add(srcCfg.toSource());
+                        } catch (Exception e) {
+                            System.err.println("Skipped one of sources from config file: " + e.getMessage());
+                        }
+                    }
+                }
+            }
+
+            if (sources.isEmpty()) {
+                System.err.println("Sources list is empty");
+                System.exit(2);
+                return;
+            }
 
             if (!propsFile.isFile()) {
                 props.save();
@@ -21,13 +66,11 @@ public class Main {
 
             props.load();
 
-            Pirocast cast = new Pirocast(Arrays
-                    .asList(new Source("FM", SignalMode.FM, 87e6f, 108e6f, 88e6f, null, true, false),
-                            new Source("AUX", SignalMode.AUX, 0, 0, 0, null, false, false)),
-                    props);
+            Pirocast cast = new Pirocast(sources, props);
 //            cast.start();
         } catch (Exception e) {
             e.printStackTrace();
+            System.exit(1);
         }
     }
 }
---
+++ src/main/java/io/github/defective4/rpi/pirocast/SourceConfig.java
@@ -0,0 +1,45 @@
+package io.github.defective4.rpi.pirocast;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+public class SourceConfig {
+    private final boolean allowAPRS, allowRDS;
+    private final int minFreq, maxFreq, defaultFreq;
+    private final String name, mode, extra;
+
+    public SourceConfig(String name, String mode, String extra, boolean allowAPRS, boolean allowRDS, int minFreq,
+            int maxFreq, int defaultFreq) {
+        this.name = name;
+        this.mode = mode;
+        this.extra = extra;
+        this.allowAPRS = allowAPRS;
+        this.allowRDS = allowRDS;
+        this.minFreq = minFreq;
+        this.maxFreq = maxFreq;
+        this.defaultFreq = defaultFreq;
+    }
+
+    public String isValid() {
+        if (name == null || name.isBlank()) return "name";
+        SignalMode m;
+        try {
+            m = SignalMode.valueOf(mode.toUpperCase());
+        } catch (Exception e) {
+            return mode;
+        }
+        if (m == SignalMode.FILE || m == SignalMode.NETWORK) {
+            if (extra == null || extra.isBlank()) return "extra";
+        } else if (m.getId() != SignalMode.UNDEFINED_ID && maxFreq == 0) return "maxFreq";
+        return null;
+    }
+
+    public JsonObject toJSON() {
+        return new Gson().toJsonTree(this).getAsJsonObject();
+    }
+
+    public Source toSource() {
+        return new Source(name, SignalMode.valueOf(mode.toUpperCase()), minFreq, maxFreq, defaultFreq, extra, allowRDS,
+                allowAPRS);
+    }
+}
